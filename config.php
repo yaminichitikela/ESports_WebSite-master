20245031a2e8e06baa350356e760d44019a6fc87
Yamini Chitikela <chitikelay1@montclair.edu>
2022-12-04 19:05:40 -0500

    manager view chnages are done

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+if (!isset($_SESSION)) {
+
+session_start();
+}
+$host = "localhost"; /* Host name */
+$user = "root"; /* User */
+$password = ""; /* Password */
+$dbname = "esports_website"; /* Database name */
+
+$con = mysqli_connect($host, $user, $password,$dbname);
+// Check connection
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
